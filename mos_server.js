d6fcd1d914bd9fc606d4950546d2dbfc91b61f7f
Gavin Parker <gp14958@my.bristol.ac.uk>
2017-02-12 15:17:22 +0000

    can change schedule

--- mos_server.js
+++ mos_server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var schedule = {startPhoneAt : 7, stopPhoneAt : 23};
+
+
 app.get('/', function(req, res){
 res.send('hello phone');
 })
@@ -11,10 +14,25 @@ app.get('/status', function(req, res){
     var current_hour = date.getHours();
 
     var status = "on";
-    if(current_hour <= 7 || current_hour >= 23){
+    if(current_hour <= schedule.startPhoneAt || current_hour >= schedule.stopPhoneAt){
         status = "off";
     }
     res.send(status);
+})
+
+app.get('/schedule', function(req, res){
+    res.setHeader('content-type', 'application/json');
+    res.send(schedule);
+})
+
+app.put('/schedule', function(req, res){
+    var newSchedule = req.body;
+    if(newSchedule.startPhoneAt !== null && newSchedule.stopPhoneAt !== null){
+        schedule = newSchedule;
+        res.sendStatus(201);
+    }else{
+        res.status(400).send("Please use the format {startPhoneAt : int, stopPhoneAt : int}");
+    }
 })
 
 app.listen(3000, function(){
