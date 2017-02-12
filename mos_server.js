5e267ed3f7b614ab57217f19ffeeec6508ce6211
Gavin Parker <gp14958@my.bristol.ac.uk>
2017-02-12 15:05:31 +0000

    added status response

--- mos_server.js
+++ mos_server.js
@@ -5,6 +5,18 @@ app.get('/', function(req, res){
 res.send('hello phone');
 })
 
+app.get('/status', function(req, res){
+    res.setHeader('content-type', 'text/plain');
+    var date = new Date();
+    var current_hour = date.getHours();
+
+    var status = "on";
+    if(current_hour <= 7 || current_hour >= 11){
+        status = "off";
+    }
+    res.send(status);
+})
+
 app.listen(3000, function(){
     console.log('server listening on port 3000');
 })
